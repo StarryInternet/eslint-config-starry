e9cf796f1fa66ee9bfd42c8c4028199232920d62
Kevin Ennis <kennis84@gmail.com>
2016-05-25 10:32:03 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-starry
+
+Also requires [eslint-plugin-starry](https://github.com/StarryInternet/eslint-plugin-starry)
---
+++ rules/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  'root': true,
+  'env': {
+    'es6': true
+  },
+  'plugins': [
+    'starry'
+  ],
+  'rules': {
+    'no-delete-var': 2,
+    'no-shadow-restricted-names': 2,
+    'no-unused-vars': 2,
+    'no-use-before-define': [ 2, 'nofunc' ],
+    'no-var': 2,
+    'one-var': [ 2, 'never' ],
+
+    'indent': [ 2, 2, { 'SwitchCase': 1 } ],
+
+    'quotes': [ 2, 'single' ],
+
+    'semi': [ 2, 'always' ],
+    'comma-dangle': [ 2, 'never' ],
+
+    'array-bracket-spacing': [ 2, 'always' ],
+    'block-spacing': [ 2, 'always' ],
+    'brace-style': [ 2, '1tbs', { 'allowSingleLine': true } ],
+    'comma-spacing': [ 2, { 'before': false, 'after': true } ],
+    'comma-style': [ 2, 'last' ],
+    'starry/computed-property-spacing': [ 2, 'always' ],
+    'eol-last': [ 2, 'unix' ],
+    'key-spacing': [ 2, { 'beforeColon' : false, 'afterColon': true, mode: 'minimum' } ],
+    'linebreak-style': [ 2, 'unix' ],
+    'no-spaced-func': 2,
+    'no-trailing-spaces': 2,
+    'object-curly-spacing': [ 2, 'always' ],
+    'semi-spacing': [ 2, { 'before': false, 'after': true } ],
+    'space-before-blocks': [ 2, 'always' ],
+    'space-before-function-paren': [ 2, 'never' ],
+    'keyword-spacing': [ 2, { before: true, after: true } ],
+    'starry/space-in-parens': [ 2, 'always', { 'exceptions': [ '{}', '[]', 'empty' ] } ],
+    'max-len': [ 2, 80, 2 ],
+    'space-infix-ops': [ 2 ]
+  }
+}
