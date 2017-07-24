3d5e400bdc3c91f9eb108477911f8e1fdeb94f1c
despreston <despreston@gmail.com>
2017-07-24 14:55:04 -0400

    moving jshint rules to eslint

--- rules/config.js
+++ rules/config.js
@@ -1,26 +1,29 @@
 module.exports = {
   'root': true,
   'env': {
+    'node': true,
+    'browser': true,
     'es6': true
   },
   'plugins': [
     'starry'
   ],
   'rules': {
+    'no-undef': 2,
+    'block-scoped-var': 2,
+    'no-eq-null': 2,
+    'eqeqeq': 2,
+    'curly': 2,
     'no-delete-var': 2,
     'no-shadow-restricted-names': 2,
     'no-unused-vars': 2,
     'no-use-before-define': [ 2, 'nofunc' ],
     'no-var': 2,
     'one-var': [ 2, 'never' ],
-
     'indent': [ 2, 2, { 'SwitchCase': 1, 'MemberExpression': 0 } ],
-
-    'quotes': [ 2, 'single', { allowTemplateLiterals: true } ],
-
+    'quotes': [ 2, 'single' ],
     'semi': [ 2, 'always' ],
     'comma-dangle': [ 2, 'never' ],
-
     'array-bracket-spacing': [ 2, 'always' ],
     'block-spacing': [ 2, 'always' ],
     'brace-style': [ 2, '1tbs', { 'allowSingleLine': true } ],
@@ -33,14 +36,12 @@ module.exports = {
     'no-spaced-func': 2,
     'no-trailing-spaces': 2,
     'object-curly-spacing': [ 2, 'always' ],
-    'object-shorthand': [ 2, 'always' ],
     'semi-spacing': [ 2, { 'before': false, 'after': true } ],
     'space-before-blocks': [ 2, 'always' ],
     'space-before-function-paren': [ 2, 'never' ],
     'keyword-spacing': [ 2, { before: true, after: true } ],
     'starry/space-in-parens': [ 2, 'always', { 'exceptions': [ '{}', '[]', 'empty' ] } ],
     'max-len': [ 2, 80, 2 ],
-    'space-infix-ops': [ 2 ],
-    'starry/aligned-requires': [ 2, 'always' ]
+    'space-infix-ops': [ 2 ]
   }
-}
+};
